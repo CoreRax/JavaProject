d0c81406bf545469f7a8874706aa34697520758d
ChengChuan <690684771@qq.com>
2015-12-15 22:22:48 +0800

    为客户端添加服务器配置文件

--- NetTest/Client/src/com/corerax/client/Client.java
+++ NetTest/Client/src/com/corerax/client/Client.java
@@ -1,16 +1,37 @@
 package com.corerax.client;
 
-import java.io.BufferedOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Properties;
 
 /**
  * socket客户端
  * Created by chengchuan on 2015/12/15.
  */
 public class Client {
+
+    private static String serverIP = null;
+    private static int serverPort = 0;
+
+    static {
+        //加载配置信息
+        loadConfig();
+    }
+    /**
+     * 加载配置信息
+     */
+    public static void loadConfig() {
+        try {
+            InputStream inStream = Client.class.getResourceAsStream("/serverAddr.properties");
+            Properties prop = new Properties();
+            prop.load(inStream);
+            serverIP = prop.getProperty("server.ip");
+            serverPort = Integer.parseInt(prop.getProperty("server.port"));
+        } catch (IOException e) {
+            throw new RuntimeException("读取配置文件异常", e);
+        }
+    }
 
     public void sendMessage(String message, Socket socket) throws IOException {
         //获取输出字节流
@@ -31,12 +52,19 @@ public class Client {
         socket.close();
     }
 
-    public static void main(String[] args) throws IOException {
 
-        Socket socket = new Socket("127.0.0.1", 8888);
+    public static void main(String[] args) {
+        Socket socket = null;
         String message = "这是发送的第二条信息";
         //发送消息
         Client client = new Client();
-        client.sendMessage(message, socket);
+        try {
+            socket = new Socket(serverIP, serverPort);
+            client.sendMessage(message, socket);
+        } catch (UnknownHostException e) {
+            System.out.println("未知的主机");
+        } catch (IOException e) {
+            System.out.println("信息发送失败");
+        }
     }
 }
